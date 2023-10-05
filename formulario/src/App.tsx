12b3c38e4032925bede77b64e31c3ce2d243fc12
almeidaairam <146780316+almeidaairam@users.noreply.github.com>
2023-10-05 00:49:08 +0000

    04/10/2023

---
+++ formulario/src/App.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import './App.css'
+
+const Form: React.FC = () => {
+  const [formData, setFormData] = useState({
+    nome: '',
+    cpf: '',
+    telefone: '',
+    email: '',
+    valorGasto: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Aqui você pode enviar os dados do cliente para onde desejar
+    console.log('Dados do cliente:', formData);
+  };
+
+  return (
+    <div >
+      <h2>Formulário do Cliente</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nome:</label>
+          <input
+            type="text"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>CPF:</label>
+          <input
+            type="text"
+            name="cpf"
+            value={formData.cpf}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Número de Telefone:</label>
+          <input
+            type="text"
+            name="telefone"
+            value={formData.telefone}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>E-mail:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Valor Gasto na Compra:</label>
+          <input
+            type="text"
+            name="valorGasto"
+            value={formData.valorGasto}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
